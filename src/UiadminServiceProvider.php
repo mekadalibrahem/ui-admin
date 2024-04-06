252ff6d6fe94d6f49830fa9cf64e265eeea06cec
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-04-06 07:39:41 +0300

    add server provider

---
+++ src/UiadminServiceProvider.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Mekadalibrahem\Uiadmin ;
+
+use Illuminate\Support\ServiceProvider ;
+
+class UiadminServiceProvider extends ServiceProvider 
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+       
+    }
+}
+
+
