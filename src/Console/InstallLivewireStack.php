0903dc14e4100c2d9abea113e1767d378b10341c
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-04-06 18:08:51 +0300

    fix publish spatie\permissions provider

--- src/Console/InstallLivewireStack.php
+++ src/Console/InstallLivewireStack.php
@@ -17,26 +17,26 @@ trait InstallLivewireStack
 
     public static  $COMPOSER_PACKAGE = [
         "livewire/livewire:^3.4",
-        "spatie/laravel-permission"=> "^6.4"
-    ];  
+        "spatie/laravel-permission:^6.4"
+    ];
 
     /**
      * install livewire   stack
      */
     public function installLivewireStack()
     {
-        // define path for livewire stup 
+        // define path for livewire stup
         $stup = __DIR__."/../../stubs/livewire";
         $this->components->info('Start install livewire stack ');
         $this->components->info('Start install required composer packages ');
         $this->requireComposerPackages(installLivewireStack::$COMPOSER_PACKAGE);
         $this->components->info('End install required composer packages ');
+        try {
+            $this->callSilent('vendor:publish', ['--tag' => 'Spatie\Permission\PermissionServiceProvider', '--force' => true]);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
 
-
-        // $this->callSilent('vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"');
-        // $this->installServiceProviderAfter('RouteServiceProvider' , 'Jenssegers\Agent\AgentServiceProvider');
-        //  // Configure Session...
-        //  $this->configureSession();
 
 
 
@@ -47,25 +47,25 @@ trait InstallLivewireStack
         });
         $this->components->info('Start Extracting files .... ');
 
-        // livewire 
+        // livewire
         (new Filesystem)->ensureDirectoryExists(app_path('Livewire'));
         (new Filesystem)->copyDirectory($stup.'/app/Livewire/Uiadmin' ,app_path('Livewire/Uiadmin'));
 
         (new Filesystem)->ensureDirectoryExists(app_path('View'));
         (new Filesystem)->copyDirectory($stup .'/app/View/Components/Uiadmin' , app_path('/View/Components/Uiadmin'));
-      
-        //resource 
+
+        //resource
         (new Filesystem)->copyDirectory($stup.'/resources' , resource_path());
 
         copy($stup.'/routes/uiadmin.php' , base_path('/routes/uiadmin.php') );
-       $this->add_routes_to_web_file();
-        $this->init_spatie_permissions_config();
+        $this->add_routes_to_web_file();
+
 
         copy($stup .'/tailwind.config.js', base_path('tailwind.config.js'));
         copy($stup .'/postcss.config.js', base_path('postcss.config.js'));
         copy($stup .'/vite.config.js', base_path('vite.config.js'));
         $this->components->info('End Extracting files  ');
-       
+
 
         $this->components->info('Installing and building Node dependencies.');
 
@@ -81,8 +81,8 @@ trait InstallLivewireStack
 
         $this->line('');
         $this->components->info('ui-admin  installed successfully.');
-    
-        
+
+
     }
 
 }
